192361f7c6fc28168153d05317fe0a1714f8a5f8
21V <futen1998@gmail.com>
2023-07-24 14:36:24 +0900

    mybatis jpa 연동 세팅 및 테스트

---
+++ java-spring/src/main/java/com/thejoeun/practice1/boilerplate1/model/entity/Member.java
@@ -0,0 +1,20 @@
+//package com.thejoeun.practice1.boilerplate1.model.entity;
+//
+//import jakarta.persistence.*;
+//import lombok.*;
+//
+//@Entity
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Getter
+//@Setter
+//public class Member {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Column(nullable = false)
+//    private String email;
+//
+//    private String password;
+//}
