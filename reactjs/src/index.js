14eb4cc942aa6a985bc076087751fe929c1c0ca3
21V <futen1998@gmail.com>
2023-07-27 13:07:26 +0900

    asd

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <HashRouter></HashRouter> */}
+    {/* <BrowserRouter basename={process.env.PUBLIC_URL}> */}
+    <BrowserRouter basename='/thejoeun-boilerplate'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
